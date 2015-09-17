5cda3b2701d85474b3b78bddf6a87e88e47f13d3
Haris Peco <snpe@snpe.rs>
2015-09-17 19:22:47 +0200

    Creating Eclipse project
    I have added the following:
    
    - Eclipse artifacts (.project, .classpath, .settings)
    The project can be imported to Eclipse as Eclipse existing project
    
    - required libraries:
    httpclient-4.2.1.jar
    httpcore-4.2.1.jar
    json-20141113.jar
    
    The clotho project built with maven - clotho-1.0-SNAPSHOT.jar
    I have added the jar in the project, but this project doesn't depend on the clotho project
    
    Issue:
    
    I wasn't be able to find the Controller.accessibility.ClothoReader class and I have created faked class in order to compile the project
    We would find this class anyhow

---
+++ src/Controller/accessibility/ClothoReader.java
@@ -0,0 +1,43 @@
+package Controller.accessibility;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import Controller.datastructures.Part;
+import Controller.datastructures.RGraph;
+import Controller.datastructures.RNode;
+import Controller.datastructures.RVector;
+import Controller.datastructures.Vector;
+
+/**
+ * 
+ * Fake class to compile the project
+ * 
+ * @author snpe
+ *
+ */
+public class ClothoReader {
+
+	public static RVector vectorImportClotho(Vector vector) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static HashMap<String, RGraph> partImportClotho(ArrayList<Part> partLibrary, HashSet<String> discouraged,
+			HashSet<String> recommended) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static ArrayList<RNode> gpsToNodesClotho(HashSet<Part> gps) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static ArrayList<String> parseTags(ArrayList<String> searchTags, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
